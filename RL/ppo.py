changes and fixes for atari: add pixel normalization wrapper b/w -1 and 1 change settings for gym atari to v4. This allows us to replicate cleanrl baseline results. Tested on pong and space invaders fix reward plotting issue where binarized rewards were being reported instead of gt reward scalars

RL/ppo.py
@@ -437,7 +437,10 @@ class PPO:
                         if dones.any():
                             dones_bool = dones.bool()
                             dones_cpu = dones_bool.cpu()
-                            self.episodic_returns.extend(self.total_rewards[dones_cpu].tolist())
+                            final_info_inds = np.nonzero(infos['final_info'])[0]
+                            total_rewards = [infos['final_info'][i]['episode']['r'][0] if 'episode' in infos['final_info'][i] else None for i in final_info_inds]
+                            total_rewards = list(filter(lambda item: item is not None, total_rewards))
+                            self.episodic_returns.extend(total_rewards)
                             self.episodic_lengths.extend(self.ep_len[dones_cpu].tolist())
                             self.total_rewards[dones_bool] = 0
                             self.ep_len[dones_bool] = 0

envs/gym_atari/atari_env.py
@@ -4,6 +4,7 @@ import torch
 
 from envs.gym_atari.wrappers.measures import *
 from envs.wrappers.common_wrappers import TorchWrapper, VecResizeObservation
+from envs.gym_atari.wrappers.image import PixelNormalization
 from stable_baselines3.common.atari_wrappers import (  # isort:skip
     ClipRewardEnv,
     EpisodicLifeEnv,
@@ -21,17 +22,20 @@ _atari_info_wrappers = {
 
 def make_env(env_name):
     def thunk():
-        env = gym.make(env_name, render_mode='rgb_array')
+        env = gym.make(env_name,
+                       render_mode='rgb_array',
+                       frameskip=1,
+                       repeat_action_probability=0.0)
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env = NoopResetEnv(env, noop_max=30)
-        # env = MaxAndSkipEnv(env, skip=4)
+        env = MaxAndSkipEnv(env, skip=4)
         env = EpisodicLifeEnv(env)
         if "FIRE" in env.unwrapped.get_action_meanings():
             env = FireResetEnv(env)
         env = ClipRewardEnv(env)
         env = gym.wrappers.GrayScaleObservation(env)
         env = gym.wrappers.FrameStack(env, 4)
-        env = _atari_info_wrappers[env_name](env)
+        # env = _atari_info_wrappers[env_name](env)
         return env
 
     return thunk
@@ -43,4 +47,5 @@ def make_vec_env(cfg):
     )
     envs = TorchWrapper(envs, device='cuda' if torch.cuda.is_available() else 'cpu')
     envs = VecResizeObservation(envs, (84, 84))
+    envs = PixelNormalization(envs)
     return envs